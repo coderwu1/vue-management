ce51f02ae401c7f4c224adf39a3e0961d83e7675
coderwu1 <2563987981@qq.com>
2020-09-16 00:30:17 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在 request 拦截器中，展示进度条 NProgress.start()
+// axios.interceptors.request.use(config => {
+//   // console.log(config)
+//   NProgress.start()
+//   config.headers.Authorization = window.sessionStorage.getItem('token')
+//   // 在最后必须 return config
+//   return config
+// })
+// // 在 response 拦截器中，隐藏进度条 NProgress.done()
+// axios.interceptors.response.use(config => {
+//   NProgress.done()
+//   return config
+// })
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,47 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option,
+  Cascader,
+  Alert,
+  Tabs,
+  TabPane,
+  Steps,
+  Step,
+  CheckboxGroup,
+  Checkbox,
+  Upload
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+Vue.prototype.$message=Message
