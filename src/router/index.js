838eef9ee4fa2f8495a629b566bfbb3057c2ff1d
coderwu1 <2563987981@qq.com>
2020-09-22 20:46:55 +0800

    完成了报表功能

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ const Params = () => import('../components/goods/Params')
 const List = () => import('../components/goods/List')
 const Add = () => import('../components/goods/Add')
 const Order = () => import('../components/order/Order')
+const Report = () => import('../components/report/Report')
 Vue.use(VueRouter)
 
 const routes = [
@@ -34,7 +35,8 @@ const routes = [
       {path:'/params',component:Params},
       {path:'/goods',component:List},
       {path:'/goods/add',component:Add},
-      {path:'/orders',component:Order}
+      {path:'/orders',component:Order},
+      {path:'/reports',component:Report}
     ]
   }
 ]
